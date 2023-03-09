c2d62d8ff18e6fb9a09a5fd5e5f548d67daa5376
ydykim <73946233+ydykim@users.noreply.github.com>
2023-03-09 20:04:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-sex
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+    ans1 = 0
+    ans2 = 0
+    for i in nums:
+        if target - i == i+1:
+            ans1 = i
+            ans2 = i+1
+            
+    return [ans1, ans2]
